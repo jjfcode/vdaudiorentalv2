e796231fbc590ff2c1bdbe0b9ee35c7c4435f3d0
John Ferlito <mix3@mix3sound.com>
2021-01-04 15:38:27 -0500

    arreglos de logos

---
+++ js/ourgearapp.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.list').click(function(){
+        const value = $(this).attr('data-filter');
+        if (value == 'all'){
+            $('.intemBox').show('1000');
+        }
+        else {
+            $('.itemBox').not('.'+value).hide('1000');
+            $('.itemBox').filter('.'+value).show('1000');
+        }
+    })
+    // add active class on selected item
+
+    $('.list').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+})
+
+
