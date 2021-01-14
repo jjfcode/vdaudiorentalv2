8d7413e3a251179f8090d7fc5056e77f177d4b5a
John Ferlito <mix3@mix3sound.com>
2021-01-14 18:32:31 -0500

    contact add

--- js/email.js
+++ js/email.js
@@ -1,15 +1,19 @@
-// Object para que funcione hay que ponerlo adentro de la funcion send, y se llama "emailMessage.fullEmail()"
+function check() {
+    var touring = document.getElementById('touring').checked;
+    var liveevent = document.getElementById('live-event').checked;
+    var corporate = document.getElementById('corporate').checked;
+    var studio = document.getElementById('studio').checked;
+    var videopro = document.getElementById('video-pro').checked;
+    var other = document.getElementById('other').checked;
 
-// var emailMessage = {
-//     name: document.getElementById('name').value,
-//     companyName: document.getElementById('companyname').value,
-//     email: document.getElementById('email').value,
-//     phone: document.getElementById('phone').value,
-//     message: document.getElementById('message').value,
-//     fullEmail : function() {
-//         return this.name + " " + this.companyName + " " + this.email + " " + this.phone + " " + this.message;
-//     }
-// }
+    if(touring==false && liveevent==false && corporate==false && studio==false && videopro==false && other==false) {
+        alert('Please One Type Of Business Need to be Check');
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 
 function send(event) {
     event.preventDefault();
@@ -24,6 +28,10 @@ function send(event) {
         "<br><h3 style='display:inline;'>Company Name: </h3>" + document.getElementById('companyname').value + 
         "<br><h3 style='display:inline;'>Email: </h3>" + document.getElementById('email').value + 
         "<br> <h3 style='display:inline;'>Phone: </h3>" + document.getElementById('phone').value + 
+        "<br> <h3 style='display:inline;'>Type Of Business: </h3>" + document.getElementById('touring').value +
+        "----" + document.getElementById('live-event').value + "----" + document.getElementById('corporate').value +
+        "----" + document.getElementById('studio').value + "----" + document.getElementById('video-pro').value +
+        "----" + document.getElementById('other').value +
         "<br><h3 style='display:inline;'>Message: </h3>" + document.getElementById('message').value,
         //El body toma como mensaje codigo html, Asi que todo lo que viene antes de la coma es lo que va aparecer en el mensaje del email
         //Usando js comun, podemos agregar html en formato de un string, seguido por lo que ingreso el usuario en el formulario
